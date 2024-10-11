40eb6814e1a67ad52d7da07a0f10803a00af174a
Morgan Jones <me@morganlabs.dev>
2024-10-11 04:46:11 +0100

    fix: Notification doesnt appear for cancelled screenshot, special workspaces are no longer always focused (so that) *file chooser* is 65% of screen size

--- roles/home/desktop/hyprland/scripts/screenshot.sh
+++ roles/home/desktop/hyprland/scripts/screenshot.sh
@@ -6,10 +6,11 @@ mkdir -p "$(dirname "$SAVE_TO")"
 ACTION="$1"
 
 grab_window() {
+    pkill slurp
     monitors="$(hyprctl -j monitors)"
     clients="$(hyprctl -j clients | jq -r '[.[] | select(.workspace.id | contains('"$(echo "$monitors" | jq -r 'map(.activeWorkspace.id) | join(",")')"'))]')"
 	boxes="$(echo "$clients" | jq -r '.[] | "\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1]) \(.title)"' | cut -f1,2 -d' ')"
-	slurp -r <<< "$boxes" 
+	slurp -r <<< "$boxes"
 }
 
 screenshot() {
@@ -20,11 +21,14 @@ screenshot() {
 
 case "$ACTION" in
 	"selection")
+        pkill slurp
         geometry="$(slurp)"
+        [[ "$?" -ne 0 ]] && exit 0
 		screenshot -g "$geometry"
 	;;
 	"window")
         geometry="$(grab_window)"
+        [[ "$?" -ne 0 ]] && exit 0
 		screenshot -g "$geometry"
 	;;
 	"all")
