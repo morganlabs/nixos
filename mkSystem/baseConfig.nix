60b4d63fbca959476e4efb769599cd2efc564ab9
Morgan Jones <me@morganlabs.dev>
2024-11-15 17:52:11 +0000

    fix: Move i18n to baseConfig

--- mkSystem/baseConfig.nix
+++ mkSystem/baseConfig.nix
@@ -1,4 +1,10 @@
-{ hostname, luksDevice }: { lib, vars, pkgs, ... }:
+{ hostname, luksDevice }:
+{
+  lib,
+  vars,
+  pkgs,
+  ...
+}:
 with lib;
 {
   boot.initrd.luks.devices = mkIf (luksDevice != "") {
@@ -8,7 +14,10 @@ with lib;
   # Basic Settings
   programs.git.enable = mkForce true;
   networking.hostName = mkForce hostname;
-  nix.settings.experimental-features = mkDefault [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = mkDefault [
+    "nix-command"
+    "flakes"
+  ];
 
   # User Config
   services.getty.autologinUser = mkIfStr (luksDevice != "") mkDefault "morgan"; # TODO: Make this only happen on first tty
@@ -16,7 +25,34 @@ with lib;
   users.users.${vars.user.username} = {
     isNormalUser = mkForce true;
     description = mkForce vars.user.name;
-    extraGroups = mkDefault [ "wheel" "networkmanager" ];
+    extraGroups = mkDefault [
+      "wheel"
+      "networkmanager"
+    ];
     shell = mkForce pkgs.zsh;
+  };
+
+  # i18n
+  time.timeZone = "Europe/London";
+  console.keyMap = "uk";
+
+  services.xserver.xkb = {
+    layout = "gb";
+    variant = "";
+  };
+
+  i18n = {
+    defaultLocale = "en_GB.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_GB.UTF-8";
+      LC_IDENTIFICATION = "en_GB.UTF-8";
+      LC_MEASUREMENT = "en_GB.UTF-8";
+      LC_MONETARY = "en_GB.UTF-8";
+      LC_NAME = "en_GB.UTF-8";
+      LC_NUMERIC = "en_GB.UTF-8";
+      LC_PAPER = "en_GB.UTF-8";
+      LC_TELEPHONE = "en_GB.UTF-8";
+      LC_TIME = "en_GB.UTF-8";
+    };
   };
 }
