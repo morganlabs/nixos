3772e36b2794fb187ecc0f726393084dde32641c
Morgan Jones <me@morganlabs.dev>
2024-11-15 19:07:38 +0000

    fix: Move git aliases from zsh config to git config

--- homeManagerModules/programs/zsh/config/aliases.nix
+++ homeManagerModules/programs/zsh/config/aliases.nix
@@ -1,37 +1,13 @@
-{
-  config,
-  osConfig,
-  lib,
-  ...
-}:
+{ osConfig, lib, ... }:
 with lib;
 let
-  aliases = mkMerge [
-    {
-      mkdir = "mkdir -p";
-      c = "clear";
-      l = "ls";
-      e = "exit";
-      src = "source $HOME/.zshrc";
-    }
-    (mkIf osConfig.programs.git.enable {
-      gi = "git init";
-      ga = "git add";
-      gaa = "git add .";
-      gc = "git commit";
-      gca = "git commit --amend --no-edit";
-      gcae = "git commit --amend";
-      gcall = "git add . && git commit";
-      gd = "git diff";
-      gs = "git status";
-      gp = "git push";
-      gl = "git log --oneline";
-      gll = "git log";
-      gco = "git checkout";
-      gfuck = "git reset --hard";
-      gbr = ''git branch --format="'%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:green) (%(committerdate:relative)) [ %(authorname) ]'"'';
-    })
-  ];
+  aliases = {
+    mkdir = "mkdir -p";
+    c = "clear";
+    l = "ls";
+    e = "exit";
+    src = "source $HOME/.zshrc";
+  };
 in
 {
   programs.zsh.shellAliases = aliases;
