f0fd417982a23603337ab5df713e0220ae7001fb
Morgan Jones <me@morganlabs.dev>
2024-11-18 18:09:53 +0000

    fix: Formatting

--- hosts/canals/home.nix
+++ hosts/canals/home.nix
@@ -1,6 +1,6 @@
 { ... }:
 {
-  wayland.windowManager.hyprland.settings.monitor = ["eDP-1,2560x1440,auto,1.6"];
+  wayland.windowManager.hyprland.settings.monitor = [ "eDP-1,2560x1440,auto,1.6" ];
 
   home.stateVersion = "24.05";
 }
