9af05a5af59a80270e24e624c83e6ed53309945d
Morgan Jones <me@morganlabs.dev>
2024-12-15 17:01:54 +0000

    feat: autostart GNOME keyring

--- modules/security/gnome-keyring.nix
+++ modules/security/gnome-keyring.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.modules.security.gnome-keyring;
 in
@@ -6,11 +11,30 @@ with lib;
 {
   options.modules.security.gnome-keyring = {
     enable = mkEnableOption "Enable security.gnome-keyring";
-    features.unlockOnLogin = mkBoolOption "Unlock GNOME Keyring on Login" true;
+    features = {
+      unlockOnLogin = mkBoolOption "Unlock GNOME Keyring on Login" true;
+      autostart = mkBoolOption "Automatically run the Keyring Daemon" true;
+    };
   };
 
   config = mkIf cfg.enable {
     services.gnome.gnome-keyring.enable = true;
     security.pam.services.login.enableGnomeKeyring = cfg.features.unlockOnLogin;
+
+    systemd.user.services.gnome-keyring = mkIf cfg.features.autostart {
+      enable = true;
+      description = "GNOME Keyring Daemon";
+      wantedBy = [ "default.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.gnome-keyring}/bin/gnome-keyring-daemon --start --components=secrets,ssh,gpg";
+        RuntimeDirectory = "keyring";
+        RuntimeDirectoryMode = "0700";
+        LimitMEMLOCK = "16380";
+        Environment = [
+          "XDG_RUNTIME_DIR=/run/user/%U"
+          "SSH_AUTH_SOCK=/run/user/%U/keyring/ssh"
+        ];
+      };
+    };
   };
 }
