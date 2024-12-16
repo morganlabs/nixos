9829348cbe0bc5c0a1ba928c497a72dd4e3f8f72
Morgan Jones <me@morganlabs.dev>
2024-12-16 09:22:38 +0000

    fix: right click menus appear in the correct place

--- modules/desktop/hyprland/config/rules.nix
+++ modules/desktop/hyprland/config/rules.nix
@@ -3,9 +3,6 @@
     windowrulev2 = [
       "suppressevent maximize, class:.*"
 
-      # General floating windows
-      "center, floating:1"
-
       # File Chooser - Floating, centred
       "float, initialTitle:(Open Files)"
       "center, initialTitle:(Open Files)"
