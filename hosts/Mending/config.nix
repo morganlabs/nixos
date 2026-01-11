a9dd4accc663c80f2e7517f12e33504bf4b0ed6c
Morgan Jones <me@morganlabs.dev>
2026-01-11 16:02:52 +0000

    fix(Mending): Now auto-disable internal laptop screen on BenQ connect

--- hosts/Mending/config.nix
+++ hosts/Mending/config.nix
@@ -25,9 +25,7 @@ with lib;
 
     programs = {
       prism-launcher.enable = true;
-      steam.enable = true;
       _1password.enable = true;
-      mangohud.enable = true;
     };
 
     desktop.hyprland = {
@@ -37,21 +35,19 @@ with lib;
           # Built-in
           name = "eDP-1";
           scale = "1.57";
-          position = "1920x528";
+          position = "9999x9999";
         }
         {
           # BenQ
           name = "DP-3";
           scale = "2";
           position = "2048x0";
-          # 2048x0
         }
         {
           # MSI
           name = "DP-2";
           scale = "1.25";
           position = "0x0";
-          # 0x0
         }
       ];
     };
@@ -66,30 +62,57 @@ with lib;
       ForwardAgent yes
   '';
 
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
   home-manager.users.${vars.user.username}.wayland.windowManager.hyprland.settings.exec-once =
     let
+      mainExternalMonitor.description = "BNQ BenQ RD280U PBR0062401Q";
+      internalMonitor.port = "eDP-1";
+
+      socat = "${pkgs.socat}/bin/socat";
+      jq = "${pkgs.jq}/bin/jq";
+
       autoTurnOffOnDisplay = pkgs.writeShellScript "turn-off-display-on-attach.sh" ''
         #!/usr/bin/env bash
 
-        displays() {
-            if [ "$1" = "monitoradded>>DP-3" ]; then
-                hyprctl keyword monitor "eDP-1,disable"
-            elif [ "$1" = "monitorremoved>>DP-3" ]; then
-                hyprctl reload
-            fi
+        check_if_connected() {
+          hyprctl monitors -j | ${jq} 'any(.[]; .description == "${mainExternalMonitor.description}")'
         }
 
-        ${pkgs.socat}/bin/socat -U - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r line; do displays "$line" && sleep 0.1; done
+        IS_ALREADY_CONNECTED="$(check_if_connected)"
+
+        if [[ "$IS_ALREADY_CONNECTED" == true ]]; then
+          hyprctl keyword monitor "${internalMonitor.port},disable"
+        fi
+
+        handle_event() {
+          local line="$1"
+          local event="''${line%%>>*}"
+          local data="''${line#*>>}"
+          local desc="$(printf '%s\n' "$data" | awk -F',' '{print $3}')"
+
+          case "$event" in
+            configreloaded)
+              IS_ALREADY_CONNECTED="$(check_if_connected)"
+              if [[ "$IS_ALREADY_CONNECTED" == true ]]; then
+                hyprctl keyword monitor "${internalMonitor.port},disable"
+              fi
+              ;;
+            monitoraddedv2)
+              if [ "$desc" = "${mainExternalMonitor.description}" ]; then
+                hyprctl keyword monitor "${internalMonitor.port},disable"
+              fi
+              ;;
+            monitorremovedv2)
+              if [ "$desc" = "${mainExternalMonitor.description}" ]; then
+                hyprctl reload
+              fi
+              ;;
+          esac
+        }
+
+        ${socat} -U - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r line; do handle_event "$line"; done
       '';
     in
-    mkAfter [
-      "${autoTurnOffOnDisplay}/turn-off-display-on-attach.sh"
-    ];
+    mkAfter [ "${autoTurnOffOnDisplay}" ];
 
   services.teamviewer.enable = true;
 
